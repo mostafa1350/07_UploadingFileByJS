de8e018519b24c949ad401a088fa42a7e7603123
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2025-01-20 20:55:04 +0330

    writing script ...
    writing script

--- FileUploading/Js/myScrpit.js
+++ FileUploading/Js/myScrpit.js
@@ -1,3 +1,18 @@
  const inputFile = document.getElementById("input-select-file");
  const btnFile = document.getElementById("btnFile");
- const spantxt = document.getElementById("span-file");
+ const spanTxt = document.getElementById("span-file");
+
+//  =============================
+// infact we call inputFile element when it's hidden
+
+btnFile.addEventListener("click",()=>{
+    inputFile.click();
+});
+
+// we want to investigate inputfile event when loading ...
+inputFile.addEventListener("change",()=>{
+    if(inputFile.value){
+        spanTxt.innerHTML = 
+                inputFile.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+    }else{spanTxt.innerHTML = "no file chosen,yet ...";}
+})
